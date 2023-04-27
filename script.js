d73b7bad9555762aaad3586071c4c42b11c488ca
Bradenyolo <lebronbraden52@gmail.com>
2023-04-27 10:15:53 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var openYT = document.querySelector("#openYT");
+
+openYT.addEventListener("click", function(){
+  window.location.href = "https://youtube.com/@bradenyolo2827?sub_confirmation=1";
+});
